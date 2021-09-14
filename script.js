ef64966a3682fd764d0254388d41eaed2fbc6499
Anthony Sellers <asell0808@yahoo.com>
2021-09-13 20:44:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 
+// Pass by value vs Pass by Reference
+//----------------------
+//Pass by value
+//----------------------
+
+var a = 5; // a now has an address of where the primitive value of 10 sits in memory 
+var b = 10; // b now has an address of where the primitive value of 10 sits in memory
+
+// primitive types are passed by value
+
+b++;
+console.log(a); //5
+console.log(b); //6
+
+var b = a;
+// passes a new value to b, which would now contain the value of 5
+// pass by value means the variables value is passed 
+// in memory based on the change of the value
+
+
+
+//------------------------
+// Pass by Reference
+//------------------------
+let obj1 = {name: 'yao', password: '123'};
+let obj2 = obj1;
+// let obj2 = ''this is the address in memory where the value is located'';
+
+obj2.password = 'easypeasy';
+
+console.log(obj1);
+console.log(obj2);
+
+// this will change the password of obj1 and obj2 to have a value of easypeasy.
+
+
+
+
+
+
+
+
